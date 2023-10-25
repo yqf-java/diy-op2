5341abc1bdaaafe9ec4ea551f31f07b3ad2abcf5
yqf-java <107611978+yqf-java@users.noreply.github.com>
2023-10-26 06:50:25 +0800

    Update fros-1.sh

--- fros-1.sh
+++ fros-1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git-full fros https://github.com/openfros/packages.git" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
